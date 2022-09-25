615890e992d4d89c49f50ae71c4653c84d7ba0aa
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-09-25 05:58:58 -0300

    Gerando construtores e getters e setters iniciais
    Foi criado os construtores em conjuntos com os getters e setters das
    classes presentes no projeto.

--- src/entities/Board.java
+++ src/entities/Board.java
@@ -1,5 +1,15 @@
 package entities;
 
 public class Board {
-
+	
+	private Card[][] cards;
+	
+	public Board() {
+		
+	}
+	
+	public Board(int rows, int columns) {
+		this.cards = new Card[rows][columns];
+	}
+	
 }
--- src/entities/Card.java
+++ src/entities/Card.java
@@ -1,5 +1,43 @@
 package entities;
 
 public class Card {
+	
+	private int number;
+	private Point point;
+	private String text;
+	
+	public Card() {
+		
+	}
 
+	public Card(int number, Point point, String text) {
+		this.number = number;
+		this.point = point;
+		this.text = text;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public Point getPoint() {
+		return point;
+	}
+
+	public void setPoint(Point point) {
+		this.point = point;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
 }
