8d4fd91c762c91b9937d2cba6e1d3d0c4a1cd34a
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-09-25 06:02:41 -0300

    Iniciando criação do método startBoard
    Foi iniciado a criação do método startBoard, que será o responsável por
    inicializar um Board com determinado número de cartões em posições
    aleatórias.
    
    Por enquanto, foi inserido apenas os números de cada cartão, seguindo o
    seguinte padrão:
    
    * Quantidade de cartões = (rows * columns) / 2
    * Ex: Quantidade de cartões = (3 * 8) / 2 --> Quantidade de cartões = 12
    * Para formar pares, posso seguir o seguinte padrão:
    |_. 12 + 0 = 12 (12 para cartão 1 e 0 para seu correspondente)
    |_. 11 + 1 = 12 ...
    |_. 10 + 2 = 12 ...
    |_. 9  + 3 = 12 ...
    ...
        1 + 11 = 12 ...
    
    * A lógica para criar esse padrão foi inserida no código.
    
    Obs: a lógica para a posição aleatória de cada cartão ainda vai ser
    escolhida. E o texto correspondente para cada cartão também.

--- src/application/Game.java
+++ src/application/Game.java
@@ -1,8 +1,11 @@
 package application;
 
+import entities.Board;
+
 public class Game {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Board board = new Board(3, 8);
 	}	
 }
--- src/entities/Board.java
+++ src/entities/Board.java
@@ -9,7 +9,29 @@ public class Board {
 	}
 	
 	public Board(int rows, int columns) {
+		this.startBoard(rows, columns);
+	}
+	
+	private void startBoard(int rows, int columns) {
+		
 		this.cards = new Card[rows][columns];
+		int pairs = (rows * columns) / 2;
+		int i = pairs, j = 0;
+		
+		for (int row = 0; row < rows; row++) {
+			for (int column = 0; column < columns; column++) {
+				
+				if (column % 2 == 0) { 
+					this.cards[row][column] = new Card(i, new Point(row, column), "");										
+					i--;
+				} else { 
+					this.cards[row][column] = new Card(j, new Point(row, column), "");
+					j++;
+				}
+				System.out.print(cards[row][column].getNumber()+" ");
+			}
+			System.out.println();
+		}
 	}
 	
 }
