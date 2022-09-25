f2c811f6859028d4a6d1e5edf85422e906934d18
pauloh-alc <pauloalencar.aluno@gmail.com>
2022-09-25 07:14:39 -0300

    Criando métodos choosePosition e contains
    O método contains verifica se dado ponto está presente ou não em uma
    lista. Já o método choosePosition escolhe uma posição pseudoaleatória na
    matriz de cards. O método contains é fundamental para o choosePosition,
    pois trata-se de uma método ajudante que impede a escolha de pontos já
    escolhidos.

--- src/entities/Board.java
+++ src/entities/Board.java
@@ -1,5 +1,9 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Board {
 	
 	private Card[][] cards;
@@ -15,6 +19,7 @@ public class Board {
 	private void startBoard(int rows, int columns) {
 		
 		this.cards = new Card[rows][columns];
+		List<Point> pointsList = new ArrayList<Point>();
 		int pairs = (rows * columns) / 2;
 		int i = pairs, j = 0;
 		
@@ -22,16 +27,47 @@ public class Board {
 			for (int column = 0; column < columns; column++) {
 				
 				if (column % 2 == 0) { 
-					this.cards[row][column] = new Card(i, new Point(row, column), "");										
+					this.cards[row][column] = new Card(i, choosePosition(pointsList, rows, columns), "");										
 					i--;
 				} else { 
-					this.cards[row][column] = new Card(j, new Point(row, column), "");
+					this.cards[row][column] = new Card(j, choosePosition(pointsList, rows, columns), "");
 					j++;
 				}
-				System.out.print(cards[row][column].getNumber()+" ");
+				System.out.print("("+cards[row][column].getPoint().getX()+","+cards[row][column].getPoint().getY()+") ");
 			}
 			System.out.println();
 		}
 	}
 	
+	private Point choosePosition(List<Point> pointsList, int rows, int columns) {
+		
+		Random random = new Random();
+		Point point;
+		boolean exist = true;
+		
+		do {
+		
+			int row = random.nextInt(rows);
+			int column = random.nextInt(columns);
+		
+			point = new Point(row, column); 
+			
+			if ( ! contains(pointsList, point)) {
+				pointsList.add(point);
+				exist = false;
+			}
+			
+		} while (exist);
+				
+		return point;
+	}
+	
+	private boolean contains(List<Point> list, Point point) {
+		for (Point p : list) {
+			if (p.getX() == point.getX() && p.getY() == point.getY()) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
